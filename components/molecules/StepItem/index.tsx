ef1651ff6efcc2c66352d79a2be9b334ba003edd
MuhammadAlvitoAditya <itoa47@gmail.com>
2022-03-07 09:18:14 +0700

    first commit

---
+++ components/molecules/StepItem/index.tsx
@@ -0,0 +1,31 @@
+export interface StepItemProps {
+  icon: 'step1' | 'step2' | 'step3';
+  title: String;
+  des1: String;
+  des2: String;
+}
+
+export default function StepItem(props: StepItemProps) {
+  const {
+    icon, title, des1, des2,
+  } = props;
+  return (
+    <div className="col-lg-4">
+      <div className="card feature-card border-0">
+        <img
+          src={`/icon/${icon}.svg`}
+          width={80}
+          height={80}
+          alt="icon step"
+          className="mb-30"
+        />
+        <p className="fw-semibold text-2xl mb-2 color-palette-1">{title}</p>
+        <p className="text-lg color-palette-1 mb-0">
+          {des1}
+          <br />
+          {des2}
+        </p>
+      </div>
+    </div>
+  );
+}
---
+++ components/organisms/Navbar/Menu.tsx
@@ -0,0 +1,28 @@
+/* eslint-disable jsx-a11y/anchor-is-valid */
+import CX from 'classnames';
+import Link from 'next/link';
+
+interface MenuProps {
+  title: string;
+  active?: boolean;
+  href: string;
+}
+
+export default function Menu(props: Partial<MenuProps>) {
+  const { title, active, href = '/' } = props;
+
+  const classTitle = CX({
+    'nav-link': true,
+    active,
+  });
+
+  return (
+    <li className="nav-item my-auto">
+      <Link href={href}>
+        <a className={classTitle} aria-current="page">
+          {title}
+        </a>
+      </Link>
+    </li>
+  );
+}
